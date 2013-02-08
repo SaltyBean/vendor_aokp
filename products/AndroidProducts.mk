1ba24cac0db159d215c886bc841a7e991e2b9f4c
SaltyBean <dave71670@gmail.com>
2013-02-07 19:02:28 -0600

    Update products/AndroidProducts.mk
    add captivate

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,13 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/a510.mk \
+    $(LOCAL_DIR)/captivate.mk \
     $(LOCAL_DIR)/d2att.mk \
     $(LOCAL_DIR)/d2tmo.mk \
     $(LOCAL_DIR)/d2usc.mk \
     $(LOCAL_DIR)/d2vzw.mk \
-    $(LOCAL_DIR)/grouper.mk \
-    $(LOCAL_DIR)/maguro.mk \
-    $(LOCAL_DIR)/mako.mk \
-    $(LOCAL_DIR)/manta.mk \
-    $(LOCAL_DIR)/tilapia.mk \
-    $(LOCAL_DIR)/toro.mk \
-    $(LOCAL_DIR)/toroplus.mk
+   
